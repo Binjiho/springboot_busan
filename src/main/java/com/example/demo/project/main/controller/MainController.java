045117b43f84c51e223442cb313255863d433818
Binjiho <jiyu1948@gmail.com>
2023-07-07 17:41:17 +0900

    spring_boot

--- src/main/java/com/example/demo/project/main/controller/MainController.java
+++ src/main/java/com/example/demo/project/main/controller/MainController.java
@@ -8,7 +8,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Controller
 @RequiredArgsConstructor
@@ -24,10 +26,136 @@ public class MainController extends BaseController {
         model.addAttribute("bannerTop", bannerTop);
         return "project/main/index";
     }
-
+    /* about */
+    /**
+     * 소개 전경 페이지
+     * @return
+     */
     @GetMapping("/about/introduce")
-    public String aboutPage(){
+    public String openAboutPage(){
         return "project/about/introduce";
+    }
+
+    /**
+     * 이용 안내 페이지
+     * @return
+     */
+    @GetMapping("/about/informationUse")
+    public String openInformationUsePage(){
+        return "project/about/informationUse";
+    }
+
+    /* rooms */
+    @GetMapping("/rooms/kp")
+    public String openRoomsKP(Model model){
+        List<Objects> bannerTop = new ArrayList<>();
+        model.addAttribute("bannerTop", bannerTop);
+        return "project/rooms/kp";
+    }
+
+    @GetMapping("/rooms/ky")
+    public String openRoomsKY(Model model){
+        List<Objects> bannerTop = new ArrayList<>();
+        model.addAttribute("bannerTop", bannerTop);
+        return "project/rooms/ky";
+    }
+
+    @GetMapping("/rooms/cw")
+    public String openRoomsCW(Model model){
+        List<Objects> bannerTop = new ArrayList<>();
+        model.addAttribute("bannerTop", bannerTop);
+        return "project/rooms/cw";
+    }
+
+    @GetMapping("/rooms/mp")
+    public String openRoomsMP(Model model){
+        List<Objects> bannerTop = new ArrayList<>();
+        model.addAttribute("bannerTop", bannerTop);
+        return "project/rooms/mp";
+    }
+
+    @GetMapping("/rooms/my")
+    public String openRoomsMY(Model model){
+        List<Objects> bannerTop = new ArrayList<>();
+        model.addAttribute("bannerTop", bannerTop);
+        return "project/rooms/my";
+    }
+
+    /* notice */
+    /**
+     * 공지사항 페이지
+     * @return
+     */
+    @GetMapping("/notice/list")
+    public String openNoticeListPage(){
+        return "project/notice/list";
+    }
+
+    /* review */
+    /**
+     * 이용후기 페이지
+     * @return
+     */
+    @GetMapping("/review/list")
+    public String openReviewListPage(){
+        return "project/review/list";
+    }
+
+    /* reservation */
+    /**
+     * 실시간 예약 조회 페이지
+     * @param model
+     * @return
+     */
+    @GetMapping("/reservation/list")
+    public String openReservationList(Model model){
+        return "project/reservation/list";
+    }
+
+    /**
+     * 예약 안내 페이지
+     * @param model
+     * @return
+     */
+    @GetMapping("/reservation/guide")
+    public String openReservationGuide(Model model){
+        return "project/reservation/guide";
+    }
+
+    /**
+     * 예약 조회 페이지
+     * @param model
+     * @return
+     */
+    @GetMapping("/reservation/check")
+    public String openReservationCheck(Model model){
+        return "project/reservation/check";
+    }
+
+    /* location */
+    /**
+     * 오시는길 페이지
+     * @return
+     */
+    @GetMapping("/location")
+    public String openLocationPage(){
+        return "project/location/location";
+    }
+
+    /* privacy */
+    @GetMapping("/privacy/personal")
+    public String openPersonalPage(){
+        return "project/privacy/personal";
+    }
+
+    @GetMapping("/privacy/useTerms")
+    public String openUseTermsPage(){
+        return "project/privacy/useTerms";
+    }
+
+    @GetMapping("/privacy/email")
+    public String openEmailPage(){
+        return "project/privacy/email";
     }
 
 }
--- src/main/java/com/example/demo/project/reservation/controller/ReservationController.java
+++ src/main/java/com/example/demo/project/reservation/controller/ReservationController.java
@@ -13,51 +13,33 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequiredArgsConstructor
 public class ReservationController extends BaseController {
 
-    @GetMapping("/reservation")
-    public String openReservationList(Model model){
-        return "project/reservation/reservation";
-    }
-
-
-//
-//    //게시판 작성 페이지
-//    @GetMapping("/admin/board/write")
-//    public String openboardWrite(@RequestParam(value = "id", required = false) final Long id, Model model) {
-//        if (id != null) {
-//            BoardResponse item = boardService.findPostById(id);
-//            model.addAttribute("item", item);
-//        }
-//        return "admin/board/write";
+//    /**
+//     * 실시간 예약 조회 페이지
+//     * @param model
+//     * @return
+//     */
+//    @GetMapping("/list")
+//    public String openReservationList(Model model){
+//        return "project/reservation/list";
 //    }
 //
-//    //게시판 생성
-//    @PostMapping("/admin/board/save")
-//    public String saveBoard(final BoardRequest params, Model model) {
-//        boardService.savePost(params);
-//        MessageDto message = new MessageDto("게시글 생성이 완료되었습니다.", "/admin/board/list", RequestMethod.GET, null);
-//        return showAlertAndRedirect(message, model);
+//    /**
+//     * 예약 안내 페이지
+//     * @param model
+//     * @return
+//     */
+//    @GetMapping("/guide")
+//    public String openReservationGuide(Model model){
+//        return "project/reservation/guide";
 //    }
 //
-//    //게시판 뷰 페이지
-//    @GetMapping("/admin/board/view")
-//    public String openBoardView(@RequestParam final Long id, Model model){
-//        BoardResponse item = boardService.findPostById(id);
-//        model.addAttribute("item", item);
-//        return "admin/board/view";
+//    /**
+//     * 예약 조회 페이지
+//     * @param model
+//     * @return
+//     */
+//    @GetMapping("/check")
+//    public String openReservationCheck(Model model){
+//        return "project/reservation/check";
 //    }
-//
-//    //게시판 수정
-//    @PostMapping("/admin/board/update")
-//    public String updateBoard(final BoardRequest params){
-//        boardService.updatePost(params);
-//        return "redirect:/admin/board/list";
-//    }
-//
-//    //게시판 삭제
-//    @PostMapping("/admin/board/delete")
-//    public String deleteBoard(@RequestParam final Long id){
-//        boardService.deletePost(id);
-//        return "redirect:/admin/board/list";
-//    }
-
 }
